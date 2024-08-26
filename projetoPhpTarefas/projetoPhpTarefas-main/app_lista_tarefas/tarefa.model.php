640c4b30678b3044301e8ecf481e6e0a98a6fd32
Rafaprimak <engs-rafaelprimak@camporeal.edu.br>
2024-08-26 15:50:33 -0300

    Atualizacao com Ordenacao

--- projetoPhpTarefas/projetoPhpTarefas-main/app_lista_tarefas/tarefa.model.php
+++ projetoPhpTarefas/projetoPhpTarefas-main/app_lista_tarefas/tarefa.model.php
@@ -4,7 +4,8 @@ class Tarefa {
 	private $id;
 	private $id_status;
 	private $tarefa;
-	private $data_cadastro;
+	private $data_cadatro;
+	private $data_cadastrado;
 
 	public function __get($atributo) {
 		return $this->$atributo;
